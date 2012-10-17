72a895baef97c1d6fa63bfaec3a8b9a5713ba1fa
Kerry Knight <kerry.knight@ppdi.com>
2012-10-17 16:06:23 -0400

    New test cases

---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,14 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by Kerry on 10/17/12.
+//  Copyright (c) 2012 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+
+@end
