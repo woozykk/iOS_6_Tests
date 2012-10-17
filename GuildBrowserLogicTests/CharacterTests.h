7d44ce3b05815e92248d8abfe48a6f6e5c2881b0
Kerry Knight <kerry.knight@ppdi.com>
2012-10-17 16:31:54 -0400

    added character test data

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Kerry on 10/17/12.
+//  Copyright (c) 2012 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
